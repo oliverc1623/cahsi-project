5fa051488eedd44eb3e5e7f30cade96d2a81be20
Oliver <oliverc1622@gmail.com>
2024-05-31 01:28:12 +0000

    faster inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -18,7 +18,6 @@ import loralib as lora
 from tqdm import tqdm
 from statistics import mean
 import monai
-numbers = re.compile(r'(\d+)')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 numbers = re.compile(r'(\d+)')
@@ -28,10 +27,9 @@ def numericalSort(value):
     return parts
 
 def create_dataset(images, labels):
+    print("Creating Dataset from dict")
     dataset = datasets.Dataset.from_dict({"image": images,
                                 "label": labels})
-    dataset = dataset.cast_column("image", datasets.Image())
-    dataset = dataset.cast_column("label", datasets.Image())
     return dataset
 
 def process_data(image_file, mask=False):
@@ -61,22 +59,29 @@ def get_bounding_box(ground_truth_map):
     return bbox
 
 def calculateIoU(gtMask, predMask):
-        # Calculate the true positives,
-        # false positives, and false negatives
-        tp = 0
-        fp = 0
-        fn = 0
-        for i in range(len(gtMask)):
-            for j in range(len(gtMask[0])):
-                if gtMask[i][j] == 1 and predMask[i][j] == 1:
-                    tp += 1
-                elif gtMask[i][j] == 0 and predMask[i][j] == 1:
-                    fp += 1
-                elif gtMask[i][j] == 1 and predMask[i][j] == 0:
-                    fn += 1
-        # Calculate IoU
-        iou = tp / (tp + fp + fn)
-        return iou
+    intersection = torch.sum(predMask * gtMask)
+    union = torch.sum(predMask) + torch.sum(gtMask) - intersection
+    if union == 0:
+        iou = float('nan')
+    else:
+        iou = intersection / union
+    return iou.item()
+
+class SAMDataset(Dataset):
+    def __init__(self, dataset, processor):
+        self.dataset = dataset
+        self.processor = processor
+    def __len__(self):
+        return len(self.dataset)
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        image = item["image"]
+        ground_truth_mask = np.array(item["label"])
+        prompt = get_bounding_box(ground_truth_mask)
+        inputs = self.processor(image, input_boxes=[[prompt]], return_tensors="pt")
+        inputs = {k:v.squeeze(0) for k,v in inputs.items()}
+        inputs["ground_truth_mask"] = ground_truth_mask
+        return inputs
 
 def apply_lora(model):
     # Mask decoder
@@ -109,14 +114,13 @@ def apply_lora(model):
 
 def main():
     ## Load Test Set
-    subset_size = 100
+    subset_size = 400
     test_filelist_xray = sorted(glob.glob('../QaTa-COV19/QaTa-COV19-v2/Test Set/Images/*.png'))
-    x_test = np.array([process_data(file_xray) for file_xray in test_filelist_xray[:subset_size]])
+    x_test = [process_data(file_xray) for file_xray in test_filelist_xray[:subset_size]]
+
     test_masks = sorted(glob.glob('../QaTa-COV19/QaTa-COV19-v2/Test Set/Ground-truths/*.png'))
-    y_test = np.array([process_data(m, mask=True) for m in test_masks[:subset_size]])
-    y_test[y_test > 0] = 1 
-    print(f"Test data shape: {x_test.shape}")
-    print(f"Test labels (masks) data shape: {y_test.shape}")
+    y_test = [process_data(m, mask=True) for m in test_masks[:subset_size]]
+
     test_dataset = create_dataset(x_test, y_test)
 
     # Load model
@@ -128,24 +132,31 @@ def main():
     model.to(device)
     model.load_state_dict(torch.load("../lora-sam.pth"))
 
-    # Inference on test set
+    # Initialize Dataset and split into train and validation dataloaders
+    test_dataset = SAMDataset(dataset=test_dataset, processor=processor)
+    dataset_size = len(test_dataset)
+    test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=8)
+
     test_ious = []
-    for idx, sample in enumerate(test_dataset):
-        ground_truth_mask = np.array(sample["label"])
-        prompt = get_bounding_box(ground_truth_mask)        
-        image = sample["image"]
-        inputs = processor(image, input_boxes=[[prompt]], return_tensors="pt")
-        inputs = {k: v.to(device) for k, v in inputs.items()}
-        model.eval()
-        # forward pass
-        with torch.no_grad():
-            outputs = model(**inputs, multimask_output=False)
-        medsam_seg_prob = torch.sigmoid(outputs.pred_masks.squeeze(1))
-        medsam_seg_prob = medsam_seg_prob.cpu().numpy().squeeze()
-        medsam_seg = (medsam_seg_prob > 0.5).astype(np.uint8)
-        iou = calculateIoU(ground_truth_mask, medsam_seg)
-        test_ious.append(iou)
-    
+    model.eval()
+    with torch.no_grad():
+        for indx, batch in enumerate(test_dataloader):  # Make sure to use your validation DataLoader
+            # forward pass
+            outputs = model(pixel_values=batch["pixel_values"].to(device),
+                            input_boxes=batch["input_boxes"].to(device),
+                            multimask_output=False)
+            # compute loss
+            predicted_masks = outputs.pred_masks.squeeze(1)
+            ground_truth_masks = batch["ground_truth_mask"].float().to(device)
+
+            medsam_seg_prob = torch.sigmoid(predicted_masks)
+            medsam_seg_prob = medsam_seg_prob.squeeze()
+            medsam_seg = (medsam_seg_prob > 0.5).to(torch.uint8)
+            for i in range(len(medsam_seg)):
+                iou = calculateIoU(ground_truth_masks[i], medsam_seg[i])
+                test_ious.append(iou)
+                print(f"Test sample index: {indx}, i: {i}, iou: {iou}")
+
     print(f"Average IoUs over {subset_size} test samples: {mean(test_ious)}")
 
 if __name__ == "__main__":
